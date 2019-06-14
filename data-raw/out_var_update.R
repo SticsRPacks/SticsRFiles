2c45a9f2a3542f49586d39e0317a122ac9582db2
remi.vezy <VEZY@users.noreply.github.com>
2019-06-14 14:55:29 +0200

    Add find_STICS_var to display output variables (from output.csv)

---
+++ data-raw/out_var_update.R
@@ -0,0 +1,6 @@
+
+# This simple script update the output data available in STICS from the "output.csv" file.
+# Simply locate the "output.csv", read it, and pass the object to `usethis::use_data()`.
+out_data= read.csv2("D:/OneDrive/Travail_Postdoc/STICS/JavaSTICS-v85/config/outputs.csv",header = FALSE)
+colnames(out_data)= c('variable','details','unit','file',"type","V1","V2")
+usethis::use_data(out_data)
