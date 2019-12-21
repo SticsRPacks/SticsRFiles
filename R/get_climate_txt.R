0a2a6a2e957c8b7c32c1bc678bc9ff955e1af684
Patrice Lecharpentier <patrice.lecharpentier@inra.fr>
2019-12-21 11:04:12 +0100

    function renamed to get_climate_txt
    full climate.txt file in returned data.frame attr

--- R/get_climate_txt.R
+++ R/get_climate_txt.R
@@ -16,20 +16,20 @@
 #' @importFrom data.table fread
 #'
 #' @examples
-#'\dontrun{
 #' library(SticsRFiles)
-#' Meteo= read_met()
-#'}
+#' path <- system.file("extdata/txt/V9.0", package = "SticsRFiles")
+#' Meteo <- get_climate_txt(path)
 #'
 #' @export
 #'
-read_met= function(dirpath=getwd(), filename="climat.txt"){
-  Met= data.table::fread(file.path(dirpath,filename), data.table = F)
+get_climate_txt= function(dirpath=getwd(), filename="climat.txt"){
+  file_path <- file.path(dirpath,filename)
+  Met= data.table::fread(file_path, data.table = F)
   colnames(Met)= c("station","year","month","day","julian","ttmin","ttmax",
                    "ttrg","ttetp","ttrr","ttvent","ttpm","ttco2")
   Date= data.frame(Date=as.POSIXct(x = paste(Met$year,Met$month,Met$day, sep="-"),
                                    format = "%Y-%m-%d", tz="UTC"))
   Table_Met= cbind(Date,Met[,-c(grep("year|month|day|julian",colnames(Met)))])
-  attr(Table_Met, "file")= filename
+  attr(Table_Met, "file")= file_path
   return(Table_Met)
 }
