76d7670bdc8aa251481cd364b120e53dfc0412c0
Patrice Lecharpentier <patrice.lecharpentier@inra.fr>
2019-12-21 06:44:48 +0100

    function renamed returned status

--- R/gen_paramsti.R
+++ R/gen_paramsti.R
@@ -5,15 +5,16 @@
 #' @param par_names vector of parameters names
 #' @param par_values vector of parameters values
 #' @param file_name file name to generate (default value: param.sti)
+#'
 #' @examples
 #' \dontrun{
-#' gen_param_sti(".", c("par1","par2"), c(1,2))
-#' gen_param_sti("/path/to/stics/workspace", c("par1","par2"), c(1,2))
+#' gen_paramsti(".", c("par1","par2"), c(1,2))
+#' gen_paramsti("/path/to/stics/workspace", c("par1","par2"), c(1,2))
 #' }
 #'
 #' @export
 #'
-gen_param_sti <- function(workspace, par_names, par_values, file_name = "param.sti") {
+gen_paramsti <- function(workspace, par_names, par_values, file_name = "param.sti") {
   file_path <- file.path(workspace, file_name)
 
   if (file.exists(file_path)) {
@@ -33,6 +34,8 @@ gen_param_sti <- function(workspace, par_names, par_values, file_name = "param.s
   con <- file(file_path, method = "w+")
   write(paste0(nb_par,"\n",paste0(sprintf("%s\n%f",par_names,par_values), collapse="\n")), con)
   close(con)
+
+  return(invisible(TRUE))
 
 
 }
