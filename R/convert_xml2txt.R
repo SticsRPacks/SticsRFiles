8b5fa733dff9d370416df14e4b94098c2e6fe1c6
Patrice Lecharpentier <patrice.lecharpentier@inra.fr>
2019-12-24 10:42:20 +0100

    changes: help & examples

--- R/convert_xml2txt.R
+++ R/convert_xml2txt.R
@@ -3,24 +3,33 @@
 #' is converted to a text file readable by the Stics model (ficini.txt, ficplt1.txt,...)
 #' @param xml_file File path of the input xml file
 #' @param java_dir Root directory of the JavaStics installation
-#' @param plt_num The number of plants (sole crop: 1 ; mixed: 2)
-#' @param out_dir Directory of the generated file
-#' @param out_file Name of the output file
+#' @param plt_num The plant identifier (main crop: 1 ; associated crop: 2)
+#' @param out_dir Output directory of the generated file (optional, default:
+#' path of the input xml file)
+#' @param out_file Name of the output file (optional, default: fixed name for Stics)
+#'
+#' @examples
+#'\dontrun{
+#' xml_path <- "/path/to/corn_plt.xml"
+#' javastics_path <- "/path/to/javastics/root"
+#' convert_xml2txt(xml_file = xml_path, java_dir = javastics_path)
+#'}
 #'
 #' @export
 #'
 convert_xml2txt <- function(xml_file, java_dir, plt_num = 1,out_dir = NULL, out_file = NULL) {
 
-  # defining which xsl file to use according to the input xml file
-  #files_types <- c("initialisations","sols","fichierplt","fichiertec")
+  # Defining which xsl file to use according to the input xml file
   xsl_files <- c("ini2txt.xsl","sol2txt.xsl","xml2txt.xsl","xml2txt.xsl",
                  "xml2txt.xsl","xml2txt.xsl","xml2txt.xsl")
   names(xsl_files) <- c("initialisations","sols","fichierplt","fichiertec",
                         "fichiersta","fichierparamgen","fichierpar")
+  # output text file names
   files_names <- list("ficini.txt","param.sol",list("ficplt1.txt","ficplt2.txt"),
                    list("fictec1.txt","fictec2.txt"),"station.txt","tempoparv6.sti",
                    "tempopar.sti")
 
+  # Using tags from in files names for the xml file type identification
   tags = list("_ini","sols.xml","_plt","_tec","_sta","_new","_gen")
   idx = which(unlist(lapply(tags, function(x) grepl(x,xml_file))))
   calc_name = length(idx) > 0
@@ -30,7 +39,7 @@ convert_xml2txt <- function(xml_file, java_dir, plt_num = 1,out_dir = NULL, out_
     stop("Output file name not found or not provided as argument! ")
   }
 
-  # detecting plt or tec in xml file name
+  # Detecting plt or tec in xml file name
   if ( calc_name & base::is.null(out_file) ) {
     tag = tags[[idx]]
     if ( tag == "_plt" | tag == "_tec" ) {
@@ -40,15 +49,16 @@ convert_xml2txt <- function(xml_file, java_dir, plt_num = 1,out_dir = NULL, out_
     }
   }
 
-  # getting the input dir if no output dir in args
+  # Getting the input dir if no output dir in args
   if ( base::is.null(out_dir)) {
     out_dir = dirname(xml_file)
   }
 
-  # defining output path
+  # Defining output file path
   out_file_path <- file.path(out_dir,out_file)
 
-  # get the root element name for identifying the file type
+  # Getting the root element name for identifying the file type
+  # TODO: redundancy according to finding tags in files names (see above code, finding idx!)
   doc <- xml2::read_xml(xml_file)
   filet = xml2::xml_name(doc)
   style_file = file.path(java_dir,"bin/resources/xml/stylesheet",xsl_files[filet])
