cd82662494e966b85672c3da61480785fb526d6c
remi.vezy <VEZY@users.noreply.github.com>
2020-01-16 11:48:21 +0100

    Add `download_data()` to download the example data from the data repo

---
+++ R/download_data.R
@@ -0,0 +1,23 @@
+#' Download example USMs
+#'
+#' @description Download locally the example data from the [data repository](https://github.com/SticsRPacks/data)
+#' in the SticsRPacks organisation.
+#'
+#' @return Download the data and return the path to the folder where the data was downloaded
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' download_data()
+#' }
+download_data= function(dir= NULL){
+  if(is.null(dir)){
+    dir= tempdir()
+  }
+  data_dir= normalizePath(dir, winslash = "/", mustWork = FALSE)
+  data_dir_zip= normalizePath(file.path(data_dir,"master.zip"), winslash = "/", mustWork = FALSE)
+  download.file("https://github.com/SticsRPacks/data/archive/master.zip", data_dir_zip)
+  unzip(data_dir_zip, exdir = data_dir)
+  unlink(data_dir_zip)
+  normalizePath(file.path(data_dir,unzip(data_dir_zip,list = TRUE)[1,1]), winslash = "/")
+}
