61990f95694e0c958c8dee16361b10a7c03a1e7e
ThomasRobine <45353523+ThomasRobine@users.noreply.github.com>
2020-01-16 15:47:29 +0100

    Create test-get_param_gen.R

---
+++ tests/testthat/test-get_param_gen.R
@@ -0,0 +1,15 @@
+library(SticsRFiles)
+options(warn=-1)
+xml_path= file.path(system.file(package="SticsRFiles","extdata/xml/examples/V9.0/param_gen.xml"))
+context("Getting gen param values")
+
+test_that ("all values from a single node", {
+  expect_equal(unname(unlist(get_param_xml(xml_path,c("masvolcx","hcccx"),
+                                    parent_name = "Lutetian Brackish marl and limestone"))),
+               c(2.3,5))
+})
+
+test_that("getting values from choice param", {
+  expect_equal(unname(unlist(get_param_xml(xml_path,c("tnitmin","tnitopt","tnitopt2","tnitmax")))),
+               c(5,30,35,58))
+})
