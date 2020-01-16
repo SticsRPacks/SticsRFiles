25448555d72957e01c8f86bf83786ad2a465c446
Patrice Lecharpentier <patrice.lecharpentier@inra.fr>
2020-01-16 10:40:32 +0100

    new test for getting parameters names in xml files

---
+++ tests/testthat/test-get_param_names_xml.R
@@ -0,0 +1,166 @@
+library(SticsRFiles)
+options(warn=-1)
+xml_plt= file.path(system.file(package="SticsRFiles","extdata/xml/examples/V9.0/file_plt.xml"))
+xml_sols= file.path(system.file(package="SticsRFiles","extdata/xml/examples/V9.0/sols.xml"))
+xml_ini= file.path(system.file(package="SticsRFiles","extdata/xml/examples/V9.0/file_ini.xml"))
+xml_sta=file.path(system.file(package="SticsRFiles","extdata/xml/examples/V9.0/file_sta.xml"))
+xml_tec=file.path(system.file(package="SticsRFiles","extdata/xml/examples/V9.0/file_tec.xml"))
+xml_usms=file.path(system.file(package="SticsRFiles","extdata/xml/examples/V9.0/usms.xml"))
+xml_gen=file.path(system.file(package="SticsRFiles","extdata/xml/examples/V9.0/param_gen.xml"))
+xml_new=file.path(system.file(package="SticsRFiles","extdata/xml/examples/V9.0/param_newform.xml"))
+# ----------------------------------------------------------
+
+context("Getting plt param names")
+
+param_names <- unlist(get_param_names_xml(xml_plt)[[1]])
+
+test_that("option name", {
+  expect_true("codetemp" %in% param_names)
+  expect_true(all(c("codetemp", "codegdh") %in% param_names))
+})
+
+test_that("param name", {
+  expect_true("jvcmini" %in% param_names)
+  expect_true(all(c("jvcmini", "innsen") %in% param_names))
+})
+
+# ----------------------------------------------------------
+
+context("Getting sol param names")
+
+xml_sols= file.path(system.file(package="SticsRFiles","extdata/xml/examples/V9.0/sols.xml"))
+param_names <- unlist(get_param_names_xml(xml_sols)[[1]])
+
+test_that("option name", {
+  expect_true("codecailloux" %in% param_names)
+  expect_true(all(c("codecailloux", "codemacropor") %in% param_names))
+})
+
+test_that("param name", {
+  expect_true("argi" %in% param_names)
+  expect_true(all(c("argi", "profhum") %in% param_names))
+})
+
+test_that("option param name", {
+  expect_true("profimper" %in% param_names)
+  expect_true(all(c("profimper", "ksol") %in% param_names))
+})
+
+test_that("colonne name", {
+  expect_true("epc" %in% param_names)
+  expect_true(all(c("epc", "cailloux") %in% param_names))
+})
+
+# ----------------------------------------------------------
+context("Getting ini param names")
+param_names <- unlist(get_param_names_xml(xml_ini)[[1]])
+
+test_that("node name", {
+  expect_true("nbplantes" %in% param_names)
+  expect_true(all(c("nbplantes", "lai0") %in% param_names))
+})
+
+test_that("parent node name", {
+  expect_true("densinitial" %in% param_names)
+  expect_true(all(c("densinitial", "NO3init") %in% param_names))
+})
+
+# ----------------------------------------------------------
+context("Getting station param names")
+param_names <- unlist(get_param_names_xml(xml_sta)[[1]])
+
+test_that("option name", {
+  expect_true("codeetp" %in% param_names)
+  expect_true(all(c("codeetp", "codaltitude") %in% param_names))
+})
+
+test_that("param name", {
+  expect_true("zr" %in% param_names)
+  expect_true(all(c("zr", "latitude") %in% param_names))
+})
+
+test_that("option param name", {
+  expect_true("altistation" %in% param_names)
+  expect_true(all(c("altistation", "altinversion") %in% param_names))
+})
+
+
+
+# ----------------------------------------------------------
+context("Getting tec param names")
+param_names <- unlist(get_param_names_xml(xml_tec)[[1]])
+
+test_that("option name", {
+  expect_true("codetradtec" %in% param_names)
+  expect_true(all(c("codetradtec", "codedecisemis") %in% param_names))
+})
+
+test_that("param name", {
+  expect_true("iplt0" %in% param_names)
+  expect_true(all(c("iplt0", "densitesem") %in% param_names))
+})
+
+test_that("option param name", {
+  expect_true("interrang" %in% param_names)
+  expect_true(all(c("interrang", "ilev") %in% param_names))
+})
+
+
+test_that("colonne name", {
+  expect_true("coderes" %in% param_names)
+  expect_true(all(c("coderes", "julapI_or_sum_upvt") %in% param_names))
+})
+
+
+# ----------------------------------------------------------
+context("Getting usms param names")
+param_names <- unlist(get_param_names_xml(xml_usms)[[1]])
+
+
+test_that("node name", {
+  expect_true("datedebut" %in% param_names)
+  expect_true(all(c("datedebut", "fplt") %in% param_names))
+})
+
+
+# ----------------------------------------------------------
+context("Getting param_gen param names")
+param_names <- unlist(get_param_names_xml(xml_gen)[[1]])
+
+test_that("option name", {
+  expect_true("codeh2oact" %in% param_names)
+  expect_true(all(c("codeh2oact", "codeinitprec") %in% param_names))
+})
+
+test_that("param name", {
+  expect_true("proprac" %in% param_names)
+  expect_true(all(c("proprac", "daseuilbas") %in% param_names))
+})
+
+test_that("option param name", {
+  expect_true("vnitmax" %in% param_names)
+  expect_true(all(c("vnitmax", "tnitopt") %in% param_names))
+})
+
+
+# ----------------------------------------------------------
+context("Getting param_new param names")
+param_names <- unlist(get_param_names_xml(xml_new)[[1]])
+
+test_that("option name", {
+  expect_true("codetempfauche" %in% param_names)
+  expect_true(all(c("codetempfauche", "codecalferti") %in% param_names))
+})
+
+test_that("param name", {
+  expect_true("nbj_pr_apres_semis" %in% param_names)
+  expect_true(all(c("nbj_pr_apres_semis", "codetranspitalle") %in% param_names))
+})
+
+test_that("option param name", {
+  expect_true("SigmaDisTalle(1)" %in% param_names)
+  expect_true(all(c("SigmaDisTalle(1)", "ratiolN") %in% param_names))
+})
+
+
+
