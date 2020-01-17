6f8dc4242d39965fbfcf3de13be2b012c6440f7d
ThomasRobine <45353523+ThomasRobine@users.noreply.github.com>
2020-01-17 11:05:09 +0100

    Update test-get_param_usms.R

--- tests/testthat/test-get_param_usms.R
+++ tests/testthat/test-get_param_usms.R
@@ -3,9 +3,6 @@ options(warn=-1)
 xml_path= file.path(system.file(package="SticsRFiles","extdata/xml/examples/V9.0/usms.xml"))
 context("Getting usms param values")
 
-subsidiary_path1 = xml_path
-subsidiary_path2 = xml_path
-
 test_that("getting all param from an usm", {
   expect_equal(as.character(unname(unlist(get_param_xml(xml_path,c("datedebut","datefin","finit","nomsol",
                                                       "fstation","fclim1","fclim2","culturean",
@@ -13,5 +10,24 @@ test_that("getting all param from an usm", {
                                            parent_name = "usm",parent_sel_attr = "SugarCane")))),
                c(286,650,"canne_ini.xml","solcanne","climcanj_sta.xml","climcanj.1998",
                  "climcanj.1999",0,1,0))
+  expect_equal(unname(unlist(get_param_xml(xml_path,"fplt",parent_name = "plante")[[1]][1])),
+               "proto_sugarcane_plt.xml")
+  expect_equal(unname(unlist(get_param_xml(xml_path,"ftec",parent_name = "plante")[[1]][1])),
+               "canne_tec.xml")
+  expect_equal(unname(unlist(get_param_xml(xml_path,"flai",parent_name = "plante")[[1]][1])),
+               "null")
+
+  expect_equal(as.character(unname(unlist(get_param_xml(xml_path,c("datedebut","datefin","finit","nomsol",
+                                                                   "fstation","fclim1","fclim2","culturean",
+                                                                   "nbplantes","codesimul"),
+                                                        parent_name = "usm",parent_sel_attr = "potato")))),
+               c(91,250,"patate_ini.xml","solpatate","climpdtj_sta.xml","climpdtj.1997",
+                 "climpdtj.1997",1,1,0))
+  expect_equal(unname(unlist(get_param_xml(xml_path,"fplt",parent_name = "plante")[[1]][3])),
+               "proto_potato_plt.xml")
+  expect_equal(unname(unlist(get_param_xml(xml_path,"ftec",parent_name = "plante")[[1]][3])),
+               "patate_tec.xml")
+  expect_equal(unname(unlist(get_param_xml(xml_path,"flai",parent_name = "plante")[[1]][3])),
+               "null")
 })
 
