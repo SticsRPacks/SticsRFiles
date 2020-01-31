c17f340fe0b7f58554332b3a99789e5992e8e8ec
Patrice Lecharpentier <patrice.lecharpentier@inra.fr>
2020-01-31 14:56:01 +0100

    added search on usm name or a part of

--- R/get_usms_list.R
+++ R/get_usms_list.R
@@ -1,6 +1,7 @@
 #' @title Getting usms names list for an usms.xml file or a list of
 #' @description Extracting a usm names list from an usms.xml file
 #' @param usms_path usms dir path or usm file path
+#' @param name Usm name or partial name
 #' @param xml_name usms xml file name (optional, default = "usms.xml")
 #'
 #' @return A list with usms names
@@ -12,7 +13,7 @@
 #'
 #' @export
 #'
-get_usms_list <- function(usms_path,xml_name="usms.xml"){
+get_usms_list <- function(usms_path, name = NULL, xml_name="usms.xml"){
 
   # TODO: add select key: i.e. get all usms names
   # with the same soil, plant 1,...
@@ -25,5 +26,20 @@ get_usms_list <- function(usms_path,xml_name="usms.xml"){
   # Get usms list
   usms_list <- get_param_xml(usms_xml_path,"usm")
 
+  # Filter usms list with partial match
+  if (!base::is.null(name)) {
+    usms_list <- lapply(usms_list, function(x) grep_usms(x,name))
+  }
+
   return(usms_list)
 }
+
+grep_usms <- function(usms_list, name) {
+
+  usms_idx <- grep(pattern = name, x = usms_list)
+  if (!length(usms_idx)) return(NULL)
+  usms_list <- usms_list[usms_idx]
+
+}
+
+
