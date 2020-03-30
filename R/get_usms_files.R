SticsRPacks/SticsRFiles

commit 9ff4f97539e17ab157274a45b05f75ac01211406

fix issue in `get_usms_files()` + Fix CRAN check issues
Issue in `get_usms_files()`: there was an issue when the plant folder was located directly in the workspace provided by the user.

R/get_usms_files.R
@@ -1,10 +1,11 @@
 #' Getting existing xml files path list per usm from an usms.xml file
 #'
-#' @param workspace_path JavaStics workspace path
+#' @param workspace_path The path to a JavaStics-compatible workspace
 #' @param usms_list Vector of usms names (Optional)
 #' @param file_name Usms XML file name (Optional)
 #' @param file_type Vector of file(s) type to get (if not given, all types are returned, see details)
-#' @param javastics_path JavaStics installation path (Optional)
+#' @param javastics_path JavaStics installation path (Optional, needed if the plant files are not in the `workspace_path`
+#' but rather in the JavaStics default workspace)
 #'
 #' @details The possible values of file types are: "fplt", "finit", "fclim1",
 #' "fclim2", "fstation" and "ftec"
@@ -12,7 +13,9 @@
 #' @return A named list with existing files path in each usm element
 #'
 #' @export
-#' @seealso See `get_soils_list` to get all soils in a usm file.
+#' @seealso See `get_soils_list()` to get all soils in a usm file, and `get_usms_list()` to get
+#' the list of usms.
+#'
 #' @examples
 #'
 #' \dontrun{
@@ -37,9 +40,9 @@ get_usms_files <- function(workspace_path,
 
   # Types definition
   files_types <- c("fplt","finit","fclim1","fclim2","fstation", "ftec")
-  if (base::is.null(file_type)) {
+  if(is.null(file_type)){
     file_type <- files_types
-  } else {
+  }else{
     # Checking if type(s) exist(s)
   }
 
@@ -48,16 +51,17 @@ get_usms_files <- function(workspace_path,
   check_plt <- FALSE
 
   # Getting first the plant dir path from the workspace, if any
-  if ("fplt" %in% file_type) {
-    if(base::is.null(javastics_path)) stop("For getting plt files path, javastics_path is needed !")
-    ws_plant <- file.path(workspace_path, "plant")
-    if (file.exists(ws_plant)) plt_path <- ws_plant
-    check_plt <- TRUE
-  }
+  if("fplt" %in% file_type){
 
-  # Getting javastics plant path
-  if (check_plt) {
-    plt_path <- c(plt_path) # , file.path(javastics_path, "plant"))
+    if(is.null(javastics_path)){
+      plt_path <- file.path(workspace_path, "plant")
+      if(!dir.exists(plt_path)){
+        stop("plant folder not found, please add javastics_path to check in the plant files from javaStics !")
+      }
+    }else{
+      plt_path <- try(normalizePath(file.path(javastics_path,workspace_path, "plant")))
+    }
+    check_plt <- TRUE
     file_type <- setdiff(file_type, "fplt")
   }
 
@@ -65,7 +69,7 @@ get_usms_files <- function(workspace_path,
   usms_xml_path <- file.path(workspace_path, file_name)
 
   # Checking usms file
-  if (! file.exists(usms_xml_path) ) {
+  if(!file.exists(usms_xml_path)){
     stop(paste("Unknown path:", usms_xml_path))
   }
 
@@ -73,12 +77,12 @@ get_usms_files <- function(workspace_path,
   usms_full_list <- get_usms_list(usm_path = usms_xml_path)
 
   # Getting the full list or a subset
-  if ( base::is.null(usms_list) ) {
+  if(is.null(usms_list)){
     usms_list <- usms_full_list
-  } else {
+  }else{
     # checking if usms names exist
     usm_idx <- usms_full_list %in% usms_list
-    if (! length(usms_list) == sum(usm_idx) ) {
+    if(!length(usms_list) == sum(usm_idx)){
       usms_list <- usms_full_list[usm_idx]
       warning(paste("There are missing usms in usms.xml file:\n",
                     paste(usms_full_list[!usm_idx], collapse = ", ")))
@@ -89,7 +93,7 @@ get_usms_files <- function(workspace_path,
   usms_files <- vector("list", usms_nb)
 
   # Loop over usms names
-  for (i in 1:usms_nb) {
+  for(i in 1:usms_nb){
     usm_name <- usms_list[i]
     usm_files <- unlist(get_param_xml(xml_file = usms_xml_path,
                                       param_name = file_type,
@@ -98,13 +102,13 @@ get_usms_files <- function(workspace_path,
 
     usm_files <- usm_files[usm_files != "null"]
     usm_files_path <- file.path(workspace_path, usm_files)
-    files_idx <- base::file.exists(usm_files_path)
+    files_idx <- file.exists(usm_files_path)
     usm_files_path <- usm_files_path[files_idx]
     usm_files_all_exist <- length(usm_files) == length(usm_files_path)
 
     plt_files <- NULL
     plt_files_path <- NULL
-    if (check_plt) {
+    if(check_plt){
       plt_files <- unlist(get_param_xml(xml_file = usms_xml_path,
                                         param_name = "fplt",
                                         select = "usm",
@@ -113,7 +117,7 @@ get_usms_files <- function(workspace_path,
       plt_files <- plt_files[plt_files != "null"]
       # applying for multiple paths (javastics, workspace)
       plt_files_path <- unlist(lapply(plt_path, function(x) file.path(x, plt_files)))
-      plt_idx <- base::file.exists(plt_files_path)
+      plt_idx <- file.exists(plt_files_path)
       plt_files_path <- plt_files_path[plt_idx]
       plt_files_all_exist <- length(plt_files) == length(plt_files_path)
     }

R/get_daily_results.R
@@ -132,7 +132,7 @@ get_daily_results <- function(workspace,
 
     results_tbl =
       dplyr::bind_rows(Table_1, Table_2)%>%
-      dplyr::group_by(Dominance)%>%
+      dplyr::group_by(.data$Dominance)%>%
       dplyr::mutate(cum_jul= compute_doy_cumul(.data$jul, .data$ian))%>%
       dplyr::ungroup()
 

R/get_obs_int.R
@@ -14,9 +14,10 @@
 #'          If \code{mixed} is not specified (or equal to \code{NULL}), the function
 #'          tries to read the number of species from the input files.
 #'
-#' @return A data.frame (sole crop) or a list of two data.frames (mixed crops) of
-#'         the STICS-formated observations. Return \code{NULL} if no files were found,
-#'         or more files than useable.
+#' @return A data.frameof the STICS-formated observations. Return \code{NULL} if no files were found,
+#'         or more files than useable. If mixed crops (two `filename` provided), the function binds
+#'         them and add a new column called "Plant" which corresponds to the name of each observation
+#'         file.
 #'
 # @seealso \code{\link{read_output}}
 #'

man/get_obs_int.Rd
@@ -14,9 +14,10 @@ get_obs_int(dirpath = getwd(), filename = NULL, mixed = NULL)
 \item{mixed}{(optional) Is the simulation on mixed species (boolean)}
 }
 \value{
-A data.frame (sole crop) or a list of two data.frames (mixed crops) of
-the STICS-formated observations. Return \code{NULL} if no files were found,
-or more files than useable.
+A data.frameof the STICS-formated observations. Return \code{NULL} if no files were found,
+or more files than useable. If mixed crops (two \code{filename} provided), the function binds
+them and add a new column called "Plant" which corresponds to the name of each observation
+file.
 }
 \description{
 Read STICS observation file(s) for sole and/or mixed crops.

man/get_usms_files.Rd
@@ -13,7 +13,7 @@ get_usms_files(
 )
 }
 \arguments{
-\item{workspace_path}{JavaStics workspace path}
+\item{workspace_path}{The path to a JavaStics-compatible workspace}
 
 \item{usms_list}{Vector of usms names (Optional)}
 
@@ -21,7 +21,8 @@ get_usms_files(
 
 \item{file_type}{Vector of file(s) type to get (if not given, all types are returned, see details)}
 
-\item{javastics_path}{JavaStics installation path (Optional)}
+\item{javastics_path}{JavaStics installation path (Optional, needed if the plant files are not in the \code{workspace_path}
+but rather in the JavaStics default workspace)}
 }
 \value{
 A named list with existing files path in each usm element
@@ -51,5 +52,6 @@ file_type = c("finit", "ftec" ))
 
 }
 \seealso{
-See \code{get_soils_list} to get all soils in a usm file.
+See \code{get_soils_list()} to get all soils in a usm file, and \code{get_usms_list()} to get
+the list of usms.
 }

commit 3f986b78da462009a2af37ab0664372ff4c9b318

FIX: mistake in building the path for searching plant file introduced in commit 9ff4f97

R/get_usms_files.R
@@ -59,7 +59,7 @@ get_usms_files <- function(workspace_path,
         stop("plant folder not found, please add javastics_path to check in the plant files from javaStics !")
       }
     }else{
-      plt_path <- try(normalizePath(file.path(javastics_path,workspace_path, "plant")))
+      plt_path <- try(normalizePath(file.path(workspace_path, "plant")))
     }
     check_plt <- TRUE
     file_type <- setdiff(file_type, "fplt")