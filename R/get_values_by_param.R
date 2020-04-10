014cc3144f54ed0099256cbb9d7f1d6488f584ff
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2020-04-10 18:20:26 +0200

    Comments Removed recursive call with ::: !

--- R/get_values_by_param.R
+++ R/get_values_by_param.R
@@ -22,29 +22,31 @@
 #'
 get_values_by_param <- function(params_table, param_name = NULL, lines_id = NULL) {
 
+  # Getting the parameter names vector, if not given
   if ((base::is.null(param_name))) {
     param_name <- unique(gsub(pattern = "_[0-9]*$", "",x = names(params_table)))
   }
 
-  # for multiple lines
+  # For multiple lines
   if (nrow(params_table) > 1) {
-    # Lines selection
+    # Lines selection, if any id
     if (!is.null(lines_id) &&
         base:is.numeric(lines_id)
         && max(lines_id) <= nrow(params_table)) {
       params_table <- params_table[lines_id,]
     }
 
+    # Applying to each line of the data.frame
     out_list <- apply(params_table, 1,
-                      function(x) SticsRFiles ::: get_values_by_param(as.data.frame(t(x), stringsAsFactors = F ),
+                      function(x) get_values_by_param(as.data.frame(t(x), stringsAsFactors = F ),
                                                                       param_name = param_name))
     return(out_list)
   }
 
+  # Applying to a vector of param names
   if (length(param_name) > 1) {
     out_table <- lapply( param_name,
                          function(x) get_values_by_param(params_table, x))
-    #names(out_table) <- param_name
     out_table <- unlist(out_table, recursive = FALSE)
     return(out_table)
   }
@@ -58,7 +60,6 @@ get_values_by_param <- function(params_table, param_name = NULL, lines_id = NULL
     param_names <- unlist(lapply(as.character(sorted_par_id), function(x) paste(param_name,x,sep = "_")))
   }
 
-  # par_values <- suppressWarnings(as.numeric(params_table[,param_names]))
   # converting from data.frame to vector
   par_values <- unlist(params_table[,param_names])
   names(par_values) <- param_names
