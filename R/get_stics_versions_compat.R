7fca7596f2d2d7ac74e305b553c89631dd84c92e
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2020-05-11 09:32:58 +0200

    versions compat improvement with csv file info about versions and example/template files + new function for getting examples path from the installed package.

--- R/get_stics_versions_compat.R
+++ R/get_stics_versions_compat.R
@@ -6,27 +6,103 @@
 #' and the last version in use ($last_version).
 #'
 #' @examples
+#' \dontrun{
 #' get_stics_versions_compat()
 #'
+#' #> $versions_list
+#' #> [1] "V8.5" "V9.0" "V9.1"
+#' #>
+#' #> $last_version
+#' #> [1] "V9.1"
+#'}
 #'
 #' @export
 #'
+#'
+# TODO: may be in stics_versions_utils get_stics_versions_compat & new function check version
 get_stics_versions_compat <- function() {
 
-  # Getting available versions tags
-  tmpl_file <- paste0("extdata/xml/templates/")
-  versions_tags <- list.dirs(system.file(tmpl_file, package = "SticsRFiles"), full.names = F)
+  # Getting versions list
+  ver_info <- get_versions_info()
+  versions_names <- ver_info$versions
+  num_versions <- as.numeric(gsub(pattern = "^[V]","",versions_names))
 
-  # Getting versions numbers from directories names
-  versions_tags <- versions_tags[! versions_tags == "" ]
-  num_versions <- as.numeric(gsub(pattern = "^[V]","",versions_tags))
+  # Getting the last version string
+  last_version <- versions_names[ num_versions == max(num_versions) ]
 
-  # Getting the last version
-  last_version <- versions_tags[ num_versions == max(num_versions) ]
-
-  # List of versions tags ans last version tag
-  versions <- list(versions_list = versions_tags, last_version = last_version )
+  # List of versions strings ans last version string
+  versions <- list(versions_list = versions_names, last_version = last_version )
 
   return(versions)
-
 }
+
+
+
+#' Checking the validity of a given version code
+#'
+#' @param version An optional version name as listed in get_stics_versions_compat() return
+#'
+#' @return A valid version string
+#'
+#' @keywords internal
+#'
+#' @examples
+#' \dontrun{
+#'
+#' check_version_compat()
+#' [1] "V9.1"
+#'
+#' check_version_compat(version = "V8.5")
+#' [1] "V8.5"
+#'
+#' }
+check_version_compat <- function(version_name = "last") {
+
+  versions <- get_stics_versions_compat()
+  if (version_name == "last") return(versions$last_version)
+
+  if ( version_name %in% versions$versions_list) return(version_name)
+
+  stop(version_name,": is an unknown version!")
+}
+
+
+#' Getting versions data (versions strings and examples files directories list)
+#'
+#' @param version_name Optional version string (i.e. "VX.Y")
+#'
+#' @return A data.frame with versions data
+#'
+#' @keywords internal
+#'
+#' @examples
+#' \dontrun{
+#' get_versions_info()
+#'
+#' versions compat  csv  obs  sti  txt           xml       xml_tmpl   xl
+#' 1     V8.5   V8.5 V8.5 BASE BASE V8.5 examples/V8.5 templates/V8.5 BASE
+#' 2     V9.0   V9.0 V9.0 BASE BASE V9.0 examples/V9.0 templates/V9.0 BASE
+#' 3     V9.1   V9.1 V9.1 BASE BASE V9.1 examples/V9.1 templates/V9.1 BASE
+#'
+#' get_versions_info( version_name = "V8.5")
+#'
+#' versions compat  csv  obs  sti  txt           xml       xml_tmpl   xl
+#' 1     V8.5   V8.5 V8.5 BASE BASE V8.5 examples/V8.5 templates/V8.5 BASE
+#'
+#'}
+get_versions_info <- function(version_name = NULL) {
+
+  # Getting available versions info from a file
+  ver_file <- system.file("extdata/versions/stics_versions_info.csv", package = "SticsRFiles")
+  ver_info <- read.csv2(file = ver_file, stringsAsFactors = FALSE, na.strings = "")
+
+  # Returning the full data.frame for all versions
+  if (base::is.null(version_name)) return(ver_info)
+
+  # Selecting data according to the desired version
+  if (version_name %in% ver_info$versions) return(dplyr::filter(ver_info, versions == version_name ))
+
+  # Nothing to return for unknown version
+  return()
+}
+
---
+++ R/stics_examples_utils.R
@@ -0,0 +1,62 @@
+#' Getting examples files path attached to a STICS version for a given file type
+#'
+#' @param file_type A file type string among files types ("csv", "obs", "sti", "txt", "xml")
+#' @param version_name An optional version string (default: last version returned by get_stics_versions_compat())
+#'
+#' @return A directory path for examples files for given file type and STICS version
+#'
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' get_examples_path( file_type = "csv" )
+#'
+#' [1] "/path/to/user-R-library/SticsRFiles/extdata/csv/V9.1"
+#'
+#' get_examples_path( file_type = "csv", version_name = "V8.5")
+#'
+#' [1] "/path/to/user-R-library/SticsRFiles/extdata/csv/V8.5"
+#'
+#' }
+get_examples_path <- function(file_type, version_name = "last") {
+
+  # Checking file type existence
+  if (! file_type %in% get_examples_types()) stop("Unknown file_type: ", file_type)
+
+  # Validating the version string
+  version_name <- check_version_compat(version_name)
+
+  # Checking if files available for the given version
+  ver_data <- SticsRFiles:::get_versions_info(version_name = version_name)
+  if (base::is.null(ver_data)) stop("No examples avaiblable for version: ",version_name)
+
+  # Getting files dir path for the given type
+  version_dir <- ver_data[[file_type]]
+  file_str <- gsub(pattern = "(.*)_.*",x = file_type, replacement = "\\1")
+  examples_path <- system.file(file.path("extdata", file_str, version_dir), package = "SticsRFiles")
+
+  # Not existing type
+  if (examples_path == "" ) stop("Not any available ",file_type, " examples for version: ",version_name)
+
+  # Returning the examples files dir path for the given type
+  return(examples_path)
+}
+
+# TODO: evaluate if usefull ?
+# list_examples_files <- function(file_type, version_name = "last") {
+#
+#   examples_path <- get_examples_path(file_type = file_type, version_name = version_name)
+#
+#   files_list <- list.files(pattern = file_type, path = examples_path)
+#
+#   return(files_list)
+#
+# }
+
+
+get_examples_types <- function() {
+  file_types <- c("csv", "obs", "sti", "txt", "xml", "xl", "xml_tmpl")
+  return(file_types)
+}
