c2fd701cbf0e67f0dfabbf7ec8430c864a0b2194
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2020-06-22 11:12:17 +0200

    get_plants_nb: is now an interface to get either plantes nb from an xml file or a text file. Previous get_plants_nb -> get_plants_nb_xml New function: get_plants_nb_txt

--- R/get_plants_nb.R
+++ R/get_plants_nb.R
@@ -1,3 +1,46 @@
+#' @title Getting plants number per usm for all usms or selected from a usm name vector
+#'
+#' @description Extracting plant number from usms.xml or new_travail.usm file data
+#'
+#' @param usm_file_path Path of usms.xml or new_travail.usm file
+#' @param usms_list Usm(s) name(s) (optional, see details)
+#'
+#' @details Use `get_usms_list()` to get the list of the usm names for an usms.xml file.
+#'
+#' @return A named numeric vector of plants number per usm
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Xml case
+#' xml_usms <- file.path(get_examples_path( file_type = "xml"),"usms.xml")
+#' get_plants_nb(xml_usms)
+#' get_plants_nb(xml_usms,"wheat")
+#' get_plants_nb(xml_usms,c("wheat","intercrop_pea_barley"))
+#'
+#' # Txt case
+#' txt_usm <- file.path(get_examples_path( file_type = "txt"),"new_travail.usm")
+#' get_plants_nb(txt_usm)
+#'
+#' }
+#'
+#' @export
+#'
+get_plants_nb <- function(usm_file_path, usms_list=c()){
+
+  usm <- grepl(pattern = "\\.usm$", x = usm_file_path)
+  usms <- grepl(pattern = "\\.xml$", x = usm_file_path)
+
+  # Neither .usm nor .xml
+  if (! (usm | usms) ) return()
+
+  if (usm) return(get_plants_nb_txt(usm_txt_path = usm_file_path, usm_name = usms_list))
+
+  if (usms) return(get_plants_nb_xml(usm_xml_path = usm_file_path, usms_list = usms_list))
+
+}
+
+
 #' @title Getting plants number per usm, all or selected from a given list
 #'
 #' @description Extracting plant number from usms.xml file data
@@ -18,9 +61,9 @@
 #' get_plants_nb(xml_usms,c("wheat","intercrop_pea_barley"))
 #' }
 #'
-#' @export
+#' @keywords internal
 #'
-get_plants_nb <- function(usm_xml_path,usms_list=c()){
+get_plants_nb_xml <- function(usm_xml_path, usms_list=c()){
 
   # Loading xml file as xmlDocument object
   xml_usms=xmldocument(usm_xml_path)
@@ -39,3 +82,29 @@ get_plants_nb <- function(usm_xml_path,usms_list=c()){
 
   return(plants_nb)
 }
+
+
+
+
+get_plants_nb_txt <- function(usm_txt_path, usm_name = NULL) {
+
+  if (base::basename(usm_txt_path) != "new_travail.usm") return()
+
+  if (!base::file.exists(usm_txt_path)) stop(usm_txt_path, " does not exist")
+
+  if (length(usm_name) > 1) stop("Only one usm name may be given !")
+
+  # Getting usm parameters
+  usm_data <- SticsRFiles::get_usm_txt(filepath = usm_txt_path)
+
+  # Checking usm name
+  if (!base::is.null(usm_name) && usm_data$nom != usm_name) stop(usm_name, ": wrong usm name")
+
+  # Returning a named vector
+  plants_nb <- as.numeric(usm_data$nbplantes)
+  names(plants_nb) <- usm_data$nom
+  return(plants_nb)
+
+}
+
+
