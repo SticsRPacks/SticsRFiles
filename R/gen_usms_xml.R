479bb6c14c0ca6f7427a8f99994e7ed842a7b1be
Samuel Buis <samuel.buis@inrae.fr>
2021-11-18 12:00:53 +0100

    Typos in doc

--- R/gen_usms_xml.R
+++ R/gen_usms_xml.R
@@ -8,6 +8,7 @@
 #'
 #' @details Please see `get_stics_versions_compat()` for the full list of stics versions that can be used for the
 #' argument `stics_version`.
+#'
 #'  `usms_param` is a `data.frame` with the following format:
 #'
 #'  |usm_name                                  | datedebut| datefin|finit               |nomsol |fstation         |
