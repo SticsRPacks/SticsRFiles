31225d04f486cc7e51f12eed0f12c621098cac3a
Samuel Buis <samuel.buis@inra.fr>
2022-01-28 10:51:35 +0100

    Update test-set_get_param_txt.R

--- tests/testthat/test-set_get_param_txt.R
+++ tests/testthat/test-set_get_param_txt.R
@@ -2,17 +2,10 @@ library(SticsRFiles)
 
 context("Getting and Setting values from txt files")
 
-path_to_JavaStics=system.file("stics", package = "SticsRTests")
-javastics_path=file.path(path_to_JavaStics,"V90")
-
-data_dir= file.path(SticsRFiles::download_data(example_dirs="study_case_1", version_name = "V9.0"))
-stics_inputs_path=file.path(data_dir,"TxtFiles")
-dir.create(stics_inputs_path)
-javastics_workspace_path<-file.path(data_dir,"XmlFiles")
-SticsRFiles::gen_usms_xml2txt(javastics_path = javastics_path, workspace_path = javastics_workspace_path,
-                              target_path = stics_inputs_path, verbose = TRUE,
-                              usms_list="bo96iN+")
-path <- file.path(stics_inputs_path,"bo96iN+")
+path <- get_examples_path("txt")
+# Copy example to test in tempdir since the files will be modified by set_param
+file.copy(from=file.path(path,list.files(path)), to=tempdir())
+path <- tempdir()
 
 tmp1 <- get_param_txt(dirpath = path, param = "stlevamf")
 codevar <- get_param_txt(dirpath = path, param = "codevar")[[1]]
@@ -62,28 +55,28 @@ test_that ("Set and get of a varietal parameter for a unique plant for a given v
 
 
 # Now let's work on intercrops ...
-javastics_workspace_path = file.path(javastics_path,"example")
-SticsRFiles::gen_usms_xml2txt(javastics_path = javastics_path, workspace_path = javastics_workspace_path,
-                              target_path = stics_inputs_path, verbose = TRUE,
-                              usms_list="intercrop_pea_barley")
+path <- file.path(get_examples_path("txt"),"intercrop_pea_barley")
+# Copy example to test in tempdir since the files will be modified by set_param
+file.copy(from=file.path(path,list.files(path)), to=tempdir())
+path <- tempdir()
 
 # Get and modify the non-varietal parameter "forme" for the simulated variety
-tmp <- get_param_txt(dirpath = file.path(stics_inputs_path,"intercrop_pea_barley"), param = "forme")
+tmp <- get_param_txt(dirpath = path, param = "forme")
 plant <- 2
-set_param_txt(dirpath = file.path(stics_inputs_path,"intercrop_pea_barley"), param = "forme",
+set_param_txt(dirpath = path, param = "forme",
               value=as.numeric(tmp[plant])+1, plant=plant)
-tmp2 <- get_param_txt(dirpath = file.path(stics_inputs_path,"intercrop_pea_barley"), param = "forme")
+tmp2 <- get_param_txt(dirpath = path, param = "forme")
 (as.numeric(tmp[plant])+1)==as.numeric(tmp2[plant])
 test_that ("Set and get of a non-varietal parameter for an intercrop for the simulated variety", {
   expect_equal(as.numeric(tmp[plant])+1,as.numeric(tmp2[plant]))
 })
 
 # Get and modify the varietal parameter "stlevamf" for the simulated variety
-tmp <- get_param_txt(dirpath = file.path(stics_inputs_path,"intercrop_pea_barley"), param = "stlevamf")
+tmp <- get_param_txt(dirpath = path, param = "stlevamf")
 plant <- 2
-set_param_txt(dirpath = file.path(stics_inputs_path,"intercrop_pea_barley"), param = "stlevamf",
+set_param_txt(dirpath = path, param = "stlevamf",
               value=as.numeric(tmp[plant])+1, plant=2)
-tmp2 <- get_param_txt(dirpath = file.path(stics_inputs_path,"intercrop_pea_barley"), param = "stlevamf")
+tmp2 <- get_param_txt(dirpath = path, param = "stlevamf")
 test_that ("Set and get of a varietal parameter for an intercrop for the simulated variety", {
   expect_equal(as.numeric(tmp[plant])+1,as.numeric(tmp2[plant]))
 })
