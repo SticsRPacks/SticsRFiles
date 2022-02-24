13c2bf310446482cbfb57566dd4c085adffb855c
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2022-02-24 17:47:57 +0100

    added: conditionnally upgrade for general parameters files if they are located in the workspace, otherwise not necessary bcs provided in javastics config directory.

--- R/upgrade_workspace_xml.R
+++ R/upgrade_workspace_xml.R
@@ -14,7 +14,12 @@
 #'
 #' @export
 #'
-#' @details See SticsRFiles::get_stics_versions_compat() for listing versions
+#' @details
+#' - See SticsRFiles::get_stics_versions_compat() for listing versions
+#' - If general parameters files exist in `workspace`, they are also upgraded.
+#' In that case, residues parameters values are kept and might not be adapted
+#' to the target model version.
+#' - Wheather data and observations files are fully copied to `out_dir`
 #'
 #' @examples
 #' \dontrun{
@@ -39,6 +44,9 @@ upgrade_workspace_xml <- function(workspace,
   # For testing if files are upgradable
   check_version <- FALSE
   verbose <- TRUE
+  upgr_par_gen <- FALSE
+  upgr_par_new <- FALSE
+
   args <- list(...)
   if ("check_version" %in% names(args)) check_version <- args$check_version
   if ("verbose" %in% names(args)) verbose <- args$verbose
@@ -49,6 +57,7 @@ upgrade_workspace_xml <- function(workspace,
   par_gen <- get_param_gen_file(type = "param_gen.xml", workspace, javastics )
   if(is.null(par_gen)) stop("param_gen.xml: file not found in\n",
                             workspace, " or ", javastics, "directory! ")
+  if(attr(par_gen,"where") == "workspace") upgr_par_gen <- TRUE
 
   # Extracting or detecting the Stics version corresponding to the xml file
   # based on param_gen.xml file content
@@ -89,40 +98,47 @@ upgrade_workspace_xml <- function(workspace,
     if(dir.exists(tmp_path)) plant_path <- tmp_path
   }
 
-
   if(verbose) {
-    print(paste("Upgrading files from version", stics_version, "to",
-                target_version))
-    print(paste("From: ", workspace))
-    print(paste("To: ", out_dir))
+    cat(paste("Upgrading files from version", stics_version, "to",
+                target_version,"\n"))
+    cat(paste("From: ", workspace,"\n"))
+    cat(paste("To: ", out_dir,"\n"))
+    cat("-----------------------------------\n")
   }
 
   # Converting param_gen.xml
-  upgrade_param_gen_xml(file = par_gen,
-                        out_dir = out_dir,
-                        stics_version = stics_version,
-                        target_version = target_version,
-                        check_version = check_version,
-                        overwrite = overwrite)
+  if(upgr_par_gen) {
+    upgrade_param_gen_xml(file = par_gen,
+                          out_dir = out_dir,
+                          stics_version = stics_version,
+                          target_version = target_version,
+                          check_version = check_version,
+                          overwrite = overwrite)
+
+    if(verbose)
+      cat("param_gen.xml\n")
+  }
+
 
   # Getting param_newform.xml path
   par_new <- get_param_gen_file(type = "param_newform.xml", workspace, javastics )
   if(is.null(par_new)) stop("param_newform.xml: file not found in\n",
                             workspace, " or ", javastics, "directory! ")
-
-  if(verbose)
-    print("param_gen.xml")
+  if(attr(par_new,"where") == "workspace") upgr_par_new <- TRUE
 
   # Converting param_newform.xml
-  upgrade_param_newform_xml(file = par_new,
-                            out_dir = file.path(out_dir),
-                            stics_version = stics_version,
-                            target_version = target_version,
-                            check_version = check_version,
-                            param_gen_file = par_gen,
-                            overwrite = overwrite)
-  if(verbose)
-    print("param_new_form.xml")
+  if(upgr_par_new) {
+    upgrade_param_newform_xml(file = par_new,
+                              out_dir = out_dir,
+                              param_gen_file = par_gen,
+                              stics_version = stics_version,
+                              target_version = target_version,
+                              check_version = check_version,
+                              overwrite = overwrite)
+
+    if(verbose)
+      cat("param_new_form.xml\n")
+  }
 
 
   # Converting usms.xml file
@@ -130,36 +146,36 @@ upgrade_workspace_xml <- function(workspace,
 
   upgrade_usms_xml(file = usms,
                    out_dir = out_dir,
+                   param_gen_file = par_gen,
                    obs_dir = workspace,
                    stics_version = stics_version,
                    target_version = target_version,
                    check_version = check_version,
-                   param_gen_file = par_gen,
                    overwrite = overwrite)
 
   if(verbose)
-    print("usms.xml")
+    cat("usms.xml\n")
 
   # Converting sols.xml file
   sols <- file.path(workspace, "sols.xml")
 
   upgrade_sols_xml(file = sols,
                    out_dir = out_dir,
+                   param_gen_file = par_gen,
                    stics_version = stics_version,
                    target_version = target_version,
                    check_version = check_version,
-                   param_gen_file = par_gen,
                    overwrite = overwrite)
 
   if(verbose)
-    print("sols.xml")
+    cat("sols.xml\n")
 
   # Converting station files (*_sta.xml)
   sta_files <- get_in_files(in_dir_or_files = workspace, kind = "sta")
 
   upgrade_sta_xml(file = sta_files,
-                  param_gen_file = par_gen,
                   out_dir = out_dir,
+                  param_gen_file = par_gen,
                   stics_version = stics_version,
                   target_version = target_version,
                   check_version = check_version,
@@ -167,45 +183,45 @@ upgrade_workspace_xml <- function(workspace,
                   check_dir = FALSE)
 
   if(verbose)
-    print("*_sta.xml")
+    cat("*_sta.xml\n")
 
   # Converting initialisation files (*_ini.xml)
   ini_files <- get_in_files(in_dir_or_files = workspace, kind = "ini")
 
   upgrade_ini_xml(file = ini_files,
                   out_dir = out_dir,
+                  param_gen_file = par_gen,
                   stics_version = stics_version,
                   target_version = target_version,
-                  check_version = check_version,
-                  param_gen_file = par_gen,
+                  check_version = TRUE,
                   overwrite = overwrite,
                   check_dir = FALSE)
 
   if(verbose)
-    print("*_ini.xml")
+    cat("*_ini.xml\n")
 
   # Converting crop management files (*_tec.xml)
   tec_files <- get_in_files(in_dir_or_files = workspace, kind = "tec")
 
   upgrade_tec_xml(file = tec_files,
-                  param_newform_file = par_new,
                   out_dir = out_dir,
+                  param_newform_file = par_new,
+                  param_gen_file = par_gen,
                   stics_version = stics_version,
                   target_version = target_version,
                   check_version = check_version,
-                  param_gen_file = par_gen,
                   overwrite = overwrite,
                   check_dir = FALSE)
 
   if(verbose)
-    print("*_tec.xml")
+    cat("*_tec.xml\n")
 
   # Copying *.mod files (for model outputs)
   stat <- file.copy(from = list.files(path = workspace, full.names = TRUE, pattern = "*.mod"),
                     to = out_dir, overwrite = overwrite)
 
   if(verbose)
-    print("Copying *.mod files.")
+    cat("Copying *.mod files.\n")
 
   # TODO: see how to manage variables names checks in *.mod files
   # Probably, the new JavaStics path may be added as a new function argument
@@ -221,7 +237,7 @@ upgrade_workspace_xml <- function(workspace,
                     to = out_dir, overwrite = overwrite)
 
   if(verbose)
-    print("Copying *.obs files.")
+    cat("Copying *.obs files.\n")
 
   # Copying weather data files
   # Note: for the moment, all files with a numerical extension
@@ -232,37 +248,39 @@ upgrade_workspace_xml <- function(workspace,
                     to = out_dir, overwrite = overwrite)
 
   if(verbose)
-    print("Copying weather files.")
+    cat("Copying weather files.\n")
 
 
 
   # Upgrading plant files
-  # if a plant sub directory is detected in workspace
-  plant_path <- file.path(workspace, "plant")
-  if (!dir.exists(plant_path)) return()
+  # if a plant sub directory exists in workspace
+  plant_files <- get_in_files(in_dir_or_files = file.path(workspace, "plant"),
+                              kind = "plt")
 
-  plant_files <- get_in_files(in_dir_or_files = plant_path, kind = "plt")
+  if(length(plant_files) > 0) {
 
-  if(length(plant_files) == 0) return()
+    if(verbose)
+      cat("*_plt.xml\n")
+
+    # For creating a sub-directory in workspace for upgraded plant files
+    plant_out_dir <- file.path(out_dir,"plant")
+    if (!dir.exists(plant_out_dir)) dir.create(plant_out_dir)
+
+    upgrade_plt_xml(file = plant_files,
+                    out_dir = plant_out_dir,
+                    param_gen_file = par_gen,
+                    param_newform_file = par_new,
+                    stics_version = stics_version,
+                    target_version = target_version,
+                    check_version = check_version,
+                    overwrite = overwrite,
+                    check_dir = FALSE)
+  }
 
   if(verbose)
-    print("*_plt.xml")
+    cat("-----------------------------------\n")
+    cat("Files upgrade and copy is complete.\n")
 
-  # For creating a sub-directory in workspace for upgraded plant files
-  plant_out_dir <- file.path(out_dir,"plant")
-  if (!dir.exists(plant_out_dir)) dir.create(plant_out_dir)
-
-  upgrade_plt_xml(file = plant_files,
-                  out_dir = plant_out_dir,
-                  param_gen_file = par_gen,
-                  param_newform_file = par_new,
-                  stics_version = stics_version,
-                  target_version = target_version,
-                  check_version = check_version,
-                  overwrite = overwrite,
-                  check_dir = FALSE)
 
 }
-
-
 
