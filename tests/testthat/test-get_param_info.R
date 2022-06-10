d47197202e858c76107ff3014f687fb0d6cf47b9
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2022-06-10 16:21:42 +0200

    fix: content of tec file for parameters attached to codeclaircie, added case for V10.0 in tests for getting the right list of parameters when searching those containing "lai"

--- tests/testthat/test-get_param_info.R
+++ tests/testthat/test-get_param_info.R
@@ -1,18 +1,31 @@
 context("searching parameter information (get_param_info)")
 
-test_that("unknown name (should returs NULL)", {
+#stics_version <- "V9.2"
+stics_version <- get_stics_versions_compat()$latest_version
+
+test_that("unknown name (should return NULL)", {
   testthat::expect_null(get_param_info("myunknownparam"))
 })
 
 test_that("fuzzy name", {
-  lai_params <-
+  if (SticsRFiles:::get_version_num() < 10) {
+    lai_params <-
     c(
       "lai0", "codelaitr", "codlainet", "dlaimax", "dlaimaxbrut", "dlaimin",
       "laicomp", "laiplantule", "pentlaimax", "splaimax", "splaimin", "udlaimax",
       "vlaimax", "cielclair", "codeclaircie", "juleclair", "laidebeff", "laieffeuil",
       "flai"
     )
-  testthat::expect_equal(get_param_info("lai", stics_version = "V9.2")$name, lai_params)
+  } else {
+    lai_params <-
+      c(
+        "lai0", "codelaitr", "codlainet", "dlaimax", "dlaimaxbrut", "dlaimin",
+        "inilai", "laicomp", "laiplantule", "pentlaimax", "splaimax", "splaimin", "udlaimax",
+        "vlaimax", "cielclair", "codeclaircie", "juleclair", "laidebeff", "laieffeuil",
+        "flai"
+      )
+  }
+  testthat::expect_equal(get_param_info("lai", stics_version = stics_version)$name, lai_params)
 })
 
 test_that("regex name", {
@@ -29,11 +42,14 @@ test_that("regex name", {
 test_that("different stics versions", {
   v9.0 <- get_param_info("vitircarbT", stics_version = "V9.0")
   v9.2 <- get_param_info("vitircarbT", stics_version = "V9.2")
+  vlast <- get_param_info("vitircarbT", stics_version = stics_version)
 
   testthat::expect_equal(attr(v9.0, "version"), "V9.0")
   testthat::expect_equal(attr(v9.2, "version"), "V9.2")
+  testthat::expect_equal(attr(vlast, "version"), stics_version)
 
   # Set both versions to the same value for comparison:
   attr(v9.0, "version") <- attr(v9.2, "version")
   testthat::expect_equal(v9.0, v9.2)
 })
+
