28015f0cdc5092c8ebf997a2fd0f058cd3c31dfc
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2022-06-15 13:05:43 +0200

    fix args names: param, stics_version

--- R/exist_param_xml.R
+++ R/exist_param_xml.R
@@ -1,7 +1,7 @@
 #' Checking existing parameters names in XML files
 #'
-#' @param parameter Parameter name or a vector of
-#' @param version Optional, Stics version.
+#' @param param Parameter name or a vector of
+#' @param stics_version Optional, Stics version.
 #' Only the 2 latest are referenced: V9.0, V9.1 (default value)
 #'
 #' @return A logical vector of existing parameters
@@ -11,27 +11,27 @@
 #' @examples
 #' \dontrun{
 #'
-#' exist_param_xml(parameter = "albedo")
+#' exist_param_xml(param = "albedo")
 #'
-#' exist_param_xml(parameter = "albedo", version = "V9.0")
+#' exist_param_xml(param = "albedo", stics_version = "V9.0")
 #'
-#' exist_param_xml(parameter = c("albedo", "latitude", "humcapil"))
+#' exist_param_xml(param = c("albedo", "latitude", "humcapil"))
 #'
-#' exist_param_xml(parameter = c("albedo", "latitude", "humcapi"))
+#' exist_param_xml(param = c("albedo", "latitude", "humcapi"))
 #' }
 #'
-exist_param_xml <- function(parameter,
-                            version = NULL) {
+exist_param_xml <- function(param,
+                            stics_version = NULL) {
 
   # Finding exact matchs in found names
   par_names <- get_param_data_df(
-    parameter = parameter,
-    version = version,
+    param = param,
+    stics_version = stics_version,
     exact = TRUE
   )$name
 
   # Checking if any correspondence for each element of name
-  exist_status <- unlist(lapply(parameter, function(x) any(par_names %in% x)))
+  exist_status <- unlist(lapply(param, function(x) any(par_names %in% x)))
 
   # Exiting status vector
   return(exist_status)
