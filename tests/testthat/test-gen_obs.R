1ba98cadb9ea85cd36e7ea90bb7c04041e250c36
Michel Giner <michel.giner@cirad.fr>
2023-05-22 14:57:18 +0200

    Create test-gen_obs.R

---
+++ tests/testthat/test-gen_obs.R
@@ -0,0 +1,9 @@
+library(SticsRFiles)
+
+xl_path <- download_usm_xl(file = "inputs_stics_example.xlsx")
+obs_df <- read_params_table(file = xl_path, sheet_name = "Obs")
+context("Create obs file ")
+
+test_that("Obs file", {
+  expect_true(gen_obs(df = obs_df, out_dir = get_examples_path("xl")))
+})
