080d43cd5a8dfa1077a310b670c6fa78eacbcdb0
Michel Giner <michel.giner@cirad.fr>
2023-06-12 11:23:15 +0200

    Create test-compute_doy_cumul.R

---
+++ tests/testthat/test-compute_doy_cumul.R
@@ -0,0 +1,9 @@
+library(SticsRFiles)
+
+context("Compute DOY sum")
+
+
+test_that("Compute DOY", {
+  expect_no_error(SticsRFiles:::compute_doy_cumul(c(350, 360, 10, 20, 30), c( 1990, 1990, 1991,  1991, 1991 )))
+  expect_error(SticsRFiles:::compute_doy_cumul(c(350, 370, 10, 20, 30), c( 1990, 1990, 1991,  1991, 1991 )))
+})
