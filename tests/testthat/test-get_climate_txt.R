df4dcf78a5dddfaf60e0886fb1e1bcdc853b31f3
Michel Giner <michel.giner@cirad.fr>
2023-06-12 09:52:17 +0200

    Update test-get_climate_txt.R

--- tests/testthat/test-get_climate_txt.R
+++ tests/testthat/test-get_climate_txt.R
@@ -1,7 +1,5 @@
 library(SticsRFiles)
-#' library(SticsRFiles)
-#' path <- get_examples_path(file_type = "txt")
-#' Meteo <- get_climate_txt(path)
+
 stics_version <- get_stics_versions_compat()$latest_version
 version_num <- SticsRFiles:::get_version_num()
 
@@ -11,6 +9,6 @@ workspace_path <- get_examples_path("txt", stics_version = stics_version)
 
 Meteo <- get_climate_txt(workspace_path)
 
-test_that("Create a xml station file", {
+test_that("Count rows climate file", {
   expect_equal(nrow(Meteo),366)
 })
