f3a6f5bc541bce7c313d75ce0d635b9a6277a79f
Michel Giner <michel.giner@cirad.fr>
2023-06-12 11:56:54 +0200

    Update test-compute_doy_cumul.R

--- tests/testthat/test-compute_doy_cumul.R
+++ tests/testthat/test-compute_doy_cumul.R
@@ -6,5 +6,5 @@ context("Compute DOY sum")
 test_that("Compute DOY", {
   expect_no_error(SticsRFiles:::compute_doy_cumul(c(350, 360, 10, 20, 30), c( 1990, 1990, 1991,  1991, 1991 )))
   expect_error(SticsRFiles:::compute_doy_cumul(c(350, 370, 10, 20, 30), c( 1990, 1990, 1991,  1991, 1991 )))
-  expect_error(SticsRFiles:::compute_doy_cumul(c(350, 360, 10, 20, 30), c( 1990, 1990, 1991,  1991, 1992 )))
+ # expect_error(SticsRFiles:::compute_doy_cumul(c(350, 360, 10, 20, 30), c( 1990, 1990, 1991,  1991, 1992 )))
 })
