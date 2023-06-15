43be4c590d9c2fe2c365576cc1e64b8d5b1ac5c8
Michel Giner <michel.giner@cirad.fr>
2023-06-15 13:37:38 +0200

    Create test-gen_paramsti.R

---
+++ tests/testthat/test-gen_paramsti.R
@@ -0,0 +1,16 @@
+library(SticsRFiles)
+
+stics_version <- get_stics_versions_compat()$latest_version
+version_num <- SticsRFiles:::get_version_num()
+
+context("Creating an xml station file to latest version")
+
+workspace_path <- get_examples_path("xml", stics_version = stics_version)
+
+test_that("Create a xml station file", {
+  expect_true(gen_paramsti(workspace_path, c("par1", "par2"), c(1, 2)))
+  expect_error(gen_paramsti(filepath(workspace_path,"xxx"), c("par1", "par2"), c(1, 2)))
+  expect_false(gen_paramsti(workspace_path, c("par1", "par2"), c(1, 2, 3)))
+})
+
+
