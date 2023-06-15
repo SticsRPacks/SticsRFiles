6b641c27af12383930d0080b03c22a25b5253041
Michel Giner <michel.giner@cirad.fr>
2023-06-15 11:16:47 +0200

    create test_gen_varmod

--- tests/testthat/test-exist_param_xml.R
+++ tests/testthat/test-exist_param_xml.R
@@ -9,4 +9,6 @@ test_that("exist parameter", {
   expect_true(exist_param_xml("codegdh", stics_version = stics_version))
   expect_true(exist_param_xml("codephot", stics_version = stics_version))
   expect_warning(exist_param_xml("codexxx", stics_version = stics_version))
+  expect_true(exist_param_xml("code_acti_reserve",
+                              stics_version = stics_version))
 })
