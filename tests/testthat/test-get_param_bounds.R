1d84602e4d4f0b5650c7c7fba610c0027b7ceffa
Michel Giner <michel.giner@cirad.fr>
2023-06-16 08:23:22 +0200

    Create test-get_param_bounds.R

---
+++ tests/testthat/test-get_param_bounds.R
@@ -0,0 +1,17 @@
+library(SticsRFiles)
+
+stics_version <- get_stics_versions_compat()$latest_version
+version_num <- SticsRFiles:::get_version_num()
+
+context("Reaserching parameter bounds")
+
+xml_file <- file.path(get_examples_path("xml", stics_version = stics_version),"sols.xml")
+
+test_that("Reaserching parameter bounds", {
+   expect_equal(length(get_param_bounds_xml(xml_file, "profhum", "min")),2)
+   expect_equal(length(get_param_bounds_xml(xml_file, "profhum")),3)
+   expect_warning(length(get_param_bounds_xml(xml_file, "profhum")))
+   expect_equal(length(get_param_bounds_xml(xml_file, "profhum",c("min", "max"))),3)
+})
+
+
