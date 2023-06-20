64fc989d68cd283b449088466fcb1bc7b5df5d0b
Michel Giner <michel.giner@cirad.fr>
2023-06-20 08:45:39 +0200

    Update test_get_obs_txt.R

--- tests/testthat/test_get_obs_txt.R
+++ tests/testthat/test_get_obs_txt.R
@@ -6,7 +6,9 @@ version_num <- SticsRFiles:::get_version_num()
 context("Counting rows of climate file")
 
 workspace_path <- get_examples_path("obs", stics_version = stics_version)
-obs_table <- SticsRFiles:::get_obs_txt(file.path(workspace_path,"simple_example"))
+#obs_table <- SticsRFiles:::get_obs_txt(file.path(workspace_path,"simple_example"))
 test_that("Count rows climate file", {
-expect_equal(unlist(dplyr::count(obs_table),use.names=FALSE),37)
+#expect_equal(unlist(dplyr::count(obs_table),use.names=FALSE),37)
+  expect_equal(unlist(dplyr::count(get_obs_txt(file.path(workspace_path,"simple_example"))),use.names=FALSE),37)
+  expect_warning(unlist(get_obs_txt(file.path(workspace_path,"simple_example"),mixed=TRUE)))
 })
