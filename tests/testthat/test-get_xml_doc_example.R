ae961ec2ba9ac787b46047671f8aa14278205338
Michel Giner <michel.giner@cirad.fr>
2023-06-20 09:46:54 +0200

    Create test-get_xml_doc_example.R

---
+++ tests/testthat/test-get_xml_doc_example.R
@@ -0,0 +1,17 @@
+library(SticsRFiles)
+
+stics_version <- get_stics_versions_compat()$latest_version
+version_num <- SticsRFiles:::get_version_num()
+
+context("get example doc")
+usm_doc <- SticsRFiles:::get_xml_doc_example("usms.xml")
+
+test_that("get example doc", {
+  expect_equal(usm_doc@name,"usms.xml")
+})
+
+#usm_doc <- SticsRFiles:::get_xml_doc_example("usm.xml")
+
+test_that("get example doc", {
+  expect_error(SticsRFiles:::get_xml_doc_example("usm.xml"))
+})
