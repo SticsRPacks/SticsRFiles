d26a6879b6f5e177c4b2cd6e018d353ea62e873e
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2023-06-22 17:01:34 +0200

    fix: expect_no_error, line length, expected value

--- tests/testthat/test-compute_doy_cumul.R
+++ tests/testthat/test-compute_doy_cumul.R
@@ -4,7 +4,13 @@ context("Compute DOY sum")
 
 
 test_that("Compute DOY", {
-  expect_no_error(SticsRFiles:::compute_doy_cumul(c(350, 360, 10, 20, 30), c( 1990, 1990, 1991,  1991, 1991 )))
-  expect_error(SticsRFiles:::compute_doy_cumul(c(350, 370, 10, 20, 30), c( 1990, 1990, 1991,  1991, 1991 )))
- # expect_error(SticsRFiles:::compute_doy_cumul(c(350, 360, 10, 20, 30), c( 1990, 1990, 1991,  1991, 1992 )))
+  expect_equal(
+    compute_doy_cumul(c(350, 360, 10, 20, 30),
+                      c( 1990, 1990, 1991,  1991, 1991 )),
+    c(350, 360, 375, 385, 395)
+    )
+  expect_error(
+    compute_doy_cumul(c(350, 370, 10, 20, 30),
+                      c( 1990, 1990, 1991,  1991, 1991 ))
+    )
 })
--- tests/testthat/test-gen_varmod.R
+++ tests/testthat/test-gen_varmod.R
@@ -1,21 +1,24 @@
 library(SticsRFiles)
 
-stics_version <- get_stics_versions_compat()$latest_version
-version_num <- SticsRFiles:::get_version_num()
-
 context("Creating a varmod file to latest version")
 
 out_dir <- file.path(tempdir(), "varmod")
 if (!dir.exists(out_dir)) dir.create(out_dir)
 
-gen_varmod(out_dir, c("lai(n)", "hauteur"))
+gen_varmod(out_dir, "lai(n)")
 
 test_that("Create a varmod file", {
   expect_true(file.exists(file.path(out_dir, "var.mod")))
-  expect_warning(gen_varmod(out_dir, ""))
-  expect_no_error(gen_varmod(out_dir, "hauteur",append=TRUE))
-  })
-gen_varmod(out_dir, "hauteur",force=TRUE)
-test_that("Create a varmod file", {
-  expect_true(file.exists(file.path(out_dir, "var.mod")))
 })
+
+
+gen_varmod(out_dir, "hauteur",append=TRUE)
+
+test_that("Add a new variable", {
+  expect_true(
+    grep(pattern = "hauteur",
+         readLines(file.path(out_dir, "var.mod"))) > 0)
+  expect_warning(gen_varmod(out_dir, ""))
+
+  })
+
