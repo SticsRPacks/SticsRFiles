5d53ef28f3de5407806a2bc0a75ebb7f82530b14
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2023-07-27 15:05:20 +0200

    improvement of variables names conversion (still missing a case i.e. varname_1_2)

--- R/col_names_to_var.R
+++ R/col_names_to_var.R
@@ -15,22 +15,38 @@
 col_names_to_var <- function(var_list = c()) {
   . <- NULL
 
-  # for varname_n
-  var_list <- gsub("_", "(", var_list)
+  words <- strsplit(var_list, "_")
 
-  any_opening <- grepl("\\(", var_list)
+  # Getting the index of the variables to convert
+  idx_end_convert <- unlist(lapply(X = words, function(x){
+    end_conv <- grepl("[n | 0-9*]", x[length(x)])
+    if(end_conv) return(TRUE)
+    FALSE
+  }))
+
+  conv_var_list <- var_list[idx_end_convert]
+
+  # for varname_n, var_name_n,
+  # for varname_1, var_name_1,
+  # TODO: fix the case var_name_1_2
+  conv_var_list <- gsub("_(n{1}|\\d{1,2})$", "(\\1", conv_var_list)
+
+  any_opening <- grepl("\\(", conv_var_list)
 
   # Closing ) only if ( is present
-  var_list[any_opening] <- lapply(var_list[any_opening], function(x) {
+  conv_var_list[any_opening] <-
+    lapply(conv_var_list[any_opening], function(x) {
     if (!grepl(pattern = "\\)$", x)) x <- paste0(x, ")")
     return(x)
   })
 
 
   # for varname.n.
-  var_list <-
-    gsub("\\.$", ")", var_list) %>%
+  conv_var_list <-
+    gsub("\\.$", ")", conv_var_list) %>%
     gsub("\\.", "(", .)
+
+  var_list[idx_end_convert] <- conv_var_list
 
   return(var_list)
 }
