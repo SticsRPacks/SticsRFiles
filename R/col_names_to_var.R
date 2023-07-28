040b246b52705fa5e19ca2a10e2b53a9481b979b
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2023-07-28 09:51:21 +0200

    Fixed not covered cases, added tests also with the complete variables list converted as full snake case names.

--- R/col_names_to_var.R
+++ R/col_names_to_var.R
@@ -1,6 +1,7 @@
 #' @title  Convert columns names list to STICS variables names
 #' @description Change the `varname.n.` or `varname_n` by `varname(n)`
-#' other names are unchanged (i.e.: varname, varname(n), ...)
+#' or snake case names (var_name.n., var_long_name_n),
+#' (n can numeric) other names are unchanged
 #' @param var_list Columns names list
 #'
 #' @return List of STICS variables names
@@ -18,38 +19,49 @@ col_names_to_var <- function(var_list = c()) {
   words <- strsplit(var_list, "_")
 
   # Getting the index of the variables to convert
+  #
   idx_end_convert <- unlist(lapply(X = words, function(x){
-    end_conv <- grepl("[n | 0-9*]", x[length(x)])
+    end_conv <- grepl("[n | 0-9*]|nboite|nboite-1|ao|as",
+                      x[length(x)])
     if(end_conv) return(TRUE)
     FALSE
   }))
 
+  # to filter varname_12_20
+  idx_end_convert <- idx_end_convert &
+    !grepl("_\\d{1,2}_\\d{1,2}.{,1}$", var_list)
+
   # Nothing to do, t=returning the input vector
-  if(!any(idx_end_convert)) return(var_list)
-
-
-  conv_var_list <- var_list[idx_end_convert]
+  if(any(idx_end_convert)){
+    conv_var_list <- var_list[idx_end_convert]
+  } else {
+    return(var_list)
+  }
 
   # for varname_n, var_name_n,
   # for varname_1, var_name_1,
   # TODO: fix the case var_name_1_2
-  conv_var_list <- gsub("_(n{1}|\\d{1,2})$", "(\\1", conv_var_list)
+  conv_var_list <-
+    gsub("_(n{1}|\\d{1,2}|nboite|nboite-1|ao|as)$",
+         "(\\1",
+         conv_var_list)
 
   any_opening <- grepl("\\(", conv_var_list)
 
   # Closing ) only if ( is present
   conv_var_list[any_opening] <-
     lapply(conv_var_list[any_opening], function(x) {
-    if (!grepl(pattern = "\\)$", x)) x <- paste0(x, ")")
-    return(x)
-  })
+      if (!grepl(pattern = "\\)$", x)) x <- paste0(x, ")")
+      return(x)
+    })
 
 
-  # for varname.n.
+  # for varname.n., or var_name.n.
   conv_var_list <-
     gsub("\\.$", ")", conv_var_list) %>%
     gsub("\\.", "(", .)
 
+  # Setting values of the converted names
   var_list[idx_end_convert] <- conv_var_list
 
   return(var_list)
