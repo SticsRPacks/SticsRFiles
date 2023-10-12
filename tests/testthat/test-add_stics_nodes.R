8753796df3ff73814a1974a1cf7642ce67c4040c
Michel Giner <michel.giner@cirad.fr>
2023-10-12 16:02:29 +0200

    Update test-add_stics_nodes.R

--- tests/testthat/test-add_stics_nodes.R
+++ tests/testthat/test-add_stics_nodes.R
@@ -2,11 +2,12 @@ library(SticsRFiles)
 
 stics_version <- "V9.0"
 #get_stics_versions_compat()$latest_version
-tec_xml <- file.path(get_examples_path(file_type = "xml"), "file_tec.xml")
+tec_xml <- file.path(get_examples_path(file_type = "xml"),stics_version, "file_tec.xml")
 tec_doc <- SticsRFiles:::xmldocument(tec_xml)
 
+
 context("Getting and Setting values from txt files")
-SticsRFiles:::add_stics_nodes(tec_doc, "irrigation", nodes_nb = 3)
+#SticsRFiles:::add_stics_nodes(tec_doc, "irrigation", nodes_nb = 3)
 test_that("variety argument can take NULL, integer or characters", {
   expect_error (SticsRFiles:::add_stics_nodes(tec_doc, "irrigation", nodes_nb = 3))
   expect_error (SticsRFiles:::add_stics_nodes(tec_xml, "irrigation", nodes_nb = 3))
