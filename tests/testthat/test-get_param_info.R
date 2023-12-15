c4dd0c980d0dea00bbe6bf0f3a0c485351078f96
Michel Giner <michel.giner@cirad.fr>
2023-12-15 12:20:12 +0100

    Update test-get_param_info

--- tests/testthat/test-get_param_info.R
+++ tests/testthat/test-get_param_info.R
@@ -2,10 +2,34 @@ context("searching parameter information (get_param_info)")
 
 
 stics_version <- get_stics_versions_compat()$latest_version
+# creating an empty df
+# `name`,`file`,`min`,`max`, `definition`
+empty_df <- data.frame(
+  name = character(0),
+  file = character(0),
+  min = character(0),
+  max = character(0),
+  definition = character(0),
+  stringsAsFactors = FALSE
+)
 
-test_that("unknown name (should return NULL)", {
-  testthat::expect_null(get_param_info("myunknownparam"))
+# Testing empty result
+test_that("giving a unknown variable name returns a 0 row data", {
+  empty_df_var <- get_param_info("myunknownvariable",
+                               stics_version = stics_version
+  )
+  empty_df_keyword <- get_param_info(
+    keyword = "myunknownvariable",
+    stics_version = stics_version
+  )
+
+  testthat::expect_true(dplyr::all_equal(empty_df, empty_df_var))
+  testthat::expect_true(dplyr::all_equal(empty_df, empty_df_keyword))
 })
+
+# test_that("unknown name (should return NULL)", {
+#  testthat::expect_null(get_param_info("myunknownparam"))
+#})
 
 test_that("fuzzy name", {
   if (get_version_num() < 10) {
@@ -19,11 +43,20 @@ test_that("fuzzy name", {
   } else {
     lai_params <-
       c(
-        "lai0", "codelaitr", "codlainet", "dlaimax", "dlaimaxbrut", "dlaimin",
-        "inilai", "laicomp", "laiplantule", "pentlaimax", "splaimax",
-        "splaimin", "udlaimax", "vlaimax", "cielclair", "codeclaircie",
-        "juleclair", "laidebeff", "laieffeuil", "lairesiduel", "flai"
-      )
+        "cielclair", "codeclaircie", "codelaitr", "codlainet", "dlaimax",
+        "dlaimaxbrut", "dlaimin", "flai", "inilai", "juleclair", "juleclair(1)",
+        "juleclair(10)", "juleclair(2)", "juleclair(3)", "juleclair(4)",
+        "juleclair(5)", "juleclair(6)", "juleclair(7)", "juleclair(8)",
+        "juleclair(9)", "lai", "lai0", "laicomp", "laidebeff", "laieffeuil",
+        "laiplantule", "lairesiduel", "lairesiduel(1)", "lairesiduel(10)",
+        "lairesiduel(11)", "lairesiduel(12)", "lairesiduel(13)",
+        "lairesiduel(14)", "lairesiduel(15)", "lairesiduel(16)",
+        "lairesiduel(17)", "lairesiduel(18)", "lairesiduel(19)",
+        "lairesiduel(2)", "lairesiduel(20)", "lairesiduel(3)", "lairesiduel(4)",
+        "lairesiduel(5)", "lairesiduel(6)", "lairesiduel(7)", "lairesiduel(8)",
+        "lairesiduel(9)", "pentlaimax", "seuilLAIapex", "splaimax", "splaimin",
+        "udlaimax", "vlaimax"
+        )
   }
   testthat::expect_equal(get_param_info("lai",
     stics_version = stics_version
@@ -34,16 +67,22 @@ test_that("regex name", {
   testthat::expect_equal(
     get_param_info("^lai")$name,
     c(
-      "lai0", "laicomp", "laiplantule", "laidebeff", "laieffeuil", "lairesiduel"
-    )
+      "lai", "lai0", "laicomp", "laidebeff", "laieffeuil", "laiplantule",
+      "lairesiduel", "lairesiduel(1)", "lairesiduel(10)", "lairesiduel(11)",
+      "lairesiduel(12)", "lairesiduel(13)", "lairesiduel(14)",
+      "lairesiduel(15)", "lairesiduel(16)", "lairesiduel(17)",
+      "lairesiduel(18)", "lairesiduel(19)", "lairesiduel(2)", "lairesiduel(20)",
+      "lairesiduel(3)", "lairesiduel(4)", "lairesiduel(5)", "lairesiduel(6)",
+      "lairesiduel(7)", "lairesiduel(8)", "lairesiduel(9)"
+      )
   )
 })
 
 
 test_that("different STICS versions", {
-  v_90 <- get_param_info("vitircarbT", stics_version = "V9.0")
-  v_92 <- get_param_info("vitircarbT", stics_version = "V9.2")
-  vlast <- get_param_info("vitircarbT", stics_version = stics_version)
+  v_90 <- get_param_info("vlaimax", stics_version = "V9.0")
+  v_92 <- get_param_info("vlaimax", stics_version = "V9.2")
+  vlast <- get_param_info("vlaimax", stics_version = stics_version)
 
   testthat::expect_equal(attr(v_90, "version"), "V9.0")
   testthat::expect_equal(attr(v_92, "version"), "V9.2")
@@ -51,7 +90,8 @@ test_that("different STICS versions", {
 
   # Set both versions to the same value for comparison:
   attr(v_90, "version") <- attr(v_92, "version")
-  testthat::expect_equal(v_90, v_92)
+  testthat::expect_true(dplyr::all_equal(v_90, v_92))
+
   # breaking, parameter moved to cultivar parameters since V9.2
   testthat::expect_false(identical(vlast, v_92))
 })
