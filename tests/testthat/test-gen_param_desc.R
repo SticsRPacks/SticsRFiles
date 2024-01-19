186f69a6440518a587faefede9906435992081f4
Michel Giner <michel.giner@cirad.fr>
2024-01-19 15:11:26 +0100

    Get_paramdesc ++

--- tests/testthat/test-gen_param_desc.R
+++ tests/testthat/test-gen_param_desc.R
@@ -1,4 +1,5 @@
 context("Checking nb lines of inputs.csv")
 test_that("checking csv content", {
   expect_equal(dim(SticsRFiles:::get_param_desc())[1],1693)
+  expect_error(SticsRFiles:::get_param_desc("xxx"))
 })
