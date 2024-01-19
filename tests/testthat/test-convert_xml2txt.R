b6d9ba55bca52cb4150baf0bd84b8382f55a2c1f
Michel Giner <michel.giner@cirad.fr>
2024-01-19 10:07:05 +0100

    add missing out_dir test

--- tests/testthat/test-convert_xml2txt.R
+++ tests/testthat/test-convert_xml2txt.R
@@ -1,11 +1,16 @@
 stics_version <- get_stics_versions_compat()$latest_version
 
 xml_plt <- file.path(get_examples_path(file_type = "xml"), "file_plt.xml")
+convert_xml2txt(file = xml_plt)
+
+test_that("Runs if out_dir not specified", {
+  expect_true(convert_xml2txt(file = xml_plt))
+})
+
 out_dir <- file.path(tempdir(), "Test_Convert")
 if (!dir.exists(out_dir)) dir.create(out_dir)
 convert_xml2txt(file = xml_plt,out_dir = out_dir)
 txt_plt <- file.path(get_examples_path(file_type = "txt"), "ficplt1.txt")
-
 
 test_that("Convert xml file to txt file", {
   expect_true(file.exists(file.path(out_dir, "ficplt1.txt")))
