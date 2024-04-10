331b1f302b79ffe2f01f46c7eff2e2cd8fe08d52
Timothée Flutre <timothee.flutre@inrae.fr>
2024-04-10 08:27:32 +0200

    add functions to compute Julian day from year of simulation start (#66)
    * add functions to compute Julian day from year of simulation start
    
    * fix bug after remark from S. Buis
    
    * rename functions
    
    ---------
    
    Co-authored-by: Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>

---
+++ tests/testthat/test-compute_day_number.R
@@ -0,0 +1,34 @@
+
+context("Convert calendar dates to julian dates")
+
+test_that("same-year", {
+  start_date <- as.Date("2014-08-01")
+  date <- as.Date("2014-08-10")
+  expected <- 222
+  observed <- compute_day_from_date(date=date, start_date=start_date)
+  expect_equal(observed, expected)
+})
+
+test_that("two-successive-years_first-noleap_second-noleap", {
+  start_date <- as.Date("2014-08-01")
+  date <- as.Date("2015-02-10")
+  expected <- 406
+  observed <- compute_day_from_date(date=date, start_date=start_date)
+  expect_equal(observed, expected)
+})
+
+test_that("two-successive-years_first-noleap_second-leap", {
+  start_date <- as.Date("2007-08-01")
+  date <- as.Date("2008-02-10")
+  expected <- 406
+  observed <- compute_day_from_date(date=date, start_date=start_date)
+  expect_equal(observed, expected)
+})
+
+test_that("two-successive-years_first-leap_second-noleap", {
+  start_date <- as.Date("2008-08-01")
+  date <- as.Date("2009-02-10")
+  expected <- 407
+  observed <- compute_day_from_date(date=date, start_date=start_date)
+  expect_equal(observed, expected)
+})
