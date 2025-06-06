4a36c3b45dabeba0d718a226e1d7d52000e9f364
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2025-06-06 11:51:42 +0200

    refactor: remove deprecated parameters and update documentation in get_report_results, get_sim, and set_param_txt functions

--- R/get_sim.R
+++ R/get_sim.R
@@ -1,35 +1,5 @@
 #' Load and format STICS daily output file(s)
 #'
-#' @description
-#' `r lifecycle::badge("deprecated")`
-#'
-#' This function was deprecated because we realised that it's
-#' a special case of the [get_sim()]
-#'
-#' @examples
-#' \dontrun{
-#' get_daily_results(path, "banana")
-#' # ->
-#' get_sim(path, "banana")
-#' }
-#'
-#' @keywords internal
-#'
-#' @noRd
-#'
-
-get_daily_results <- function(...) {
-  lifecycle::deprecate_warn(
-    "1.0.0",
-    "get_daily_results()",
-    "get_sim()"
-  ) # ,
-  # details = "This function is a special case of get_sim(); use it instead.")
-  get_sim(...)
-}
-
-#' Load and format STICS daily output file(s)
-#'
 #' @description Reads and format daily output file(s) (mod_s*.sti) for usm(s)
 #' with possible selection on variable names, cumulative DOY and dates
 #'
@@ -50,18 +20,6 @@ get_daily_results <- function(...) {
 #' to usms_file to get the plant codes if the plant files used are not
 #' in the workspace but in the JavaSTICS distribution (see Details).
 #' @param verbose Logical value for displaying or not information while running
-#'
-#'
-#' @param usm_name `r lifecycle::badge("deprecated")` `usm_name` is no
-#' longer supported, use `usm` instead.
-#' @param var_list `r lifecycle::badge("deprecated")` `var_list` is no
-#' longer supported, use `var` instead.
-#' @param dates_list `r lifecycle::badge("deprecated")` `dates_list` is no
-#' longer supported, use `dates` instead.
-#' @param usms_filepath `r lifecycle::badge("deprecated")` `usms_filepath`
-#' is no longer supported, use `usms_file` instead.
-#' @param javastics_path `r lifecycle::badge("deprecated")` `javastics_path`
-#' is no longer supported, use `javastics` instead.
 #'
 #' @details
 #' If `usm` is not specified (or equal to `NULL`), the
@@ -90,77 +48,21 @@ get_daily_results <- function(...) {
 #' @export
 #'
 get_sim <- function(
-    workspace,
-    usm = NULL,
-    var = NULL,
-    dates = NULL,
-    usms_file = NULL,
-    javastics = NULL,
-    verbose = TRUE,
-    usm_name = lifecycle::deprecated(),
-    var_list = lifecycle::deprecated(),
-    dates_list = lifecycle::deprecated(),
-    usms_filepath = lifecycle::deprecated(),
-    javastics_path = lifecycle::deprecated()) {
-  # Managing deprecated arguments
-  # usm_name
-  if (lifecycle::is_present(usm_name)) {
-    lifecycle::deprecate_warn(
-      "1.0.0",
-      "get_sim(usm_name)",
-      "get_sim(usm)"
-    )
-  } else {
-    usm_name <- usm # to remove when we update inside the function
-  }
-  # var_list
-  if (lifecycle::is_present(var_list)) {
-    lifecycle::deprecate_warn(
-      "1.0.0",
-      "get_sim(var_list)",
-      "get_sim(var)"
-    )
-  } else {
-    var_list <- var # to remove when we update inside the function
-  }
-  # dates_list
-  if (lifecycle::is_present(dates_list)) {
-    lifecycle::deprecate_warn(
-      "1.0.0",
-      "get_sim(dates_list)",
-      "get_sim(dates)"
-    )
-  } else {
-    dates_list <- dates # to remove when we update inside the function
-  }
-  # usms_filepath
-  if (lifecycle::is_present(usms_filepath)) {
-    lifecycle::deprecate_warn(
-      "1.0.0",
-      "get_sim(usms_filepath)",
-      "get_sim(usms_file)"
-    )
-  } else {
-    usms_filepath <- usms_file # to remove when we update inside the function
-  }
-  # javastics_path
-  if (lifecycle::is_present(javastics_path)) {
-    lifecycle::deprecate_warn(
-      "1.0.0",
-      "get_sim(javastics_path)",
-      "get_sim(javastics)"
-    )
-  } else {
-    javastics_path <- javastics # to remove when we update inside the function
-  }
-
+  workspace,
+  usm = NULL,
+  var = NULL,
+  dates = NULL,
+  usms_file = NULL,
+  javastics = NULL,
+  verbose = TRUE
+) {
   res <- get_file(
     workspace,
-    usm_name,
-    var_list,
-    dates_list,
-    usms_filepath,
-    javastics_path,
+    usm,
+    var,
+    dates,
+    usms_file,
+    javastics,
     verbose,
     "sim"
   )
