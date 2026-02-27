e357dfe0dd7158ea24427cc289f5cf2904e3913c
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2026-02-27 13:06:35 +0100

    fix: removed switch use causing an error in the function examples' run

--- R/read_params_table.R
+++ R/read_params_table.R
@@ -1,6 +1,6 @@
 #' Getting parameters data from tables files (Excel sheet, csv)
 #'
-#' @param file Excel or csv file path (including name of the file)
+#' @param file_path Excel or csv file path (including name of the file)
 #' @param sheet_name Name of an Excel sheet (useless for csv files)
 #' @param num_na Replacement value for numerical NA values (default: NA)
 #' @param char_na Replacement value for character NA values (default: "")
@@ -14,7 +14,7 @@
 #' @examples
 #'
 #' usm_xl_file <- download_usm_xl(
-#'   file = "inputs_stics_example.xlsx",
+#'   file_name = "inputs_stics_example.xlsx",
 #'   verbose = FALSE
 #' )
 #' read_params_table(usm_xl_file, sheet = "USMs")
@@ -23,9 +23,10 @@
 #'   verbose = FALSE,
 #'   stics_version = "V9.2"
 #' )
-#' read_params_table(file = usm_csv_file)
+#' read_params_table(file_path = usm_csv_file)
+#'
 read_params_table <- function(
-  file,
+  file_path,
   sheet_name = NULL,
   num_na = "NA",
   char_na = "NA"
@@ -34,7 +35,7 @@ read_params_table <- function(
   files_ext_lst <- c("csv", "xls", "xlsx")
 
   # Getting file extension, and checking its validity
-  file_ext <- tools::file_ext(file)
+  file_ext <- tools::file_ext(file_path)
   if (isFALSE(file_ext %in% files_ext_lst)) {
     stop(paste0('"', file_ext, '"'), ": is not a valid extension")
   }
@@ -43,7 +44,7 @@ read_params_table <- function(
   # dfault value for csv file
   sheet_exists <- TRUE
   if (isFALSE("csv" == file_ext)) {
-    xl_sheets <- readxl::excel_sheets(file)
+    xl_sheets <- readxl::excel_sheets(file_path)
     sheet_exists <- sheet_name %in% xl_sheets
   }
   # If the sheet name is not provided
@@ -60,28 +61,24 @@ read_params_table <- function(
     return()
   }
 
-  # Reading file according to its format
-  switch(file_ext,
-    csv = {
-      out_table <- utils::read.csv2(
-        file = file,
-        header = TRUE,
-        sep = ";",
-        stringsAsFactors = FALSE,
-        na.strings = "",
-        strip.white = TRUE,
-        colClasses = "character"
-      )
-    },
-    {
-      out_table <- readxl::read_excel(
-        file,
-        sheet = sheet_name,
-        trim_ws = TRUE,
-        col_types = "text"
-      )
-    }
-  )
+  if (file_ext == "csv") {
+    out_table <- utils::read.csv2(
+      file = file_path,
+      header = TRUE,
+      sep = ";",
+      stringsAsFactors = FALSE,
+      na.strings = "",
+      strip.white = TRUE,
+      colClasses = "character"
+    )
+  } else {
+    out_table <- readxl::read_excel(
+      file_path,
+      sheet = sheet_name,
+      trim_ws = TRUE,
+      col_types = "text"
+    )
+  }
 
   # Converting if necessary to tibble object
   out_table <- tibble::as_tibble(out_table)
@@ -116,7 +113,8 @@ replace_na <- function(in_df, replacement) {
   }
 
   # Getting columns ids according to rep_type
-  switch(rep_type,
+  switch(
+    rep_type,
     numeric = {
       idx_type_col <- unlist(lapply(in_df, is.numeric), use.names = FALSE)
     },
