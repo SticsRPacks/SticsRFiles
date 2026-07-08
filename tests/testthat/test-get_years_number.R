0b6b330aa01d3cc62554f4e37d9a48366bede811
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2026-07-08 12:26:35 +0200

    fix: added checks on input files vector (length, name, extension) in function and added tests for it

---
+++ tests/testthat/test-get_years_number.R
@@ -0,0 +1,29 @@
+context("Get years number for a usm from files name")
+
+data_dir <- download_data(
+  example_dirs = "study_case_1",
+  stics_version = "V11.0",
+  raise_error = TRUE,
+  out_dir = "/tmp"
+)
+
+workspace <- file.path(data_dir, "XmlFiles")
+
+files_path <- file.path(
+  workspace,
+  c("lusignaj.1996", "lusignaj.1997", "lusignaj.abcd")
+)
+file.copy(files_path[2], files_path[3])
+
+test_that("single file path", {
+  expect_error(get_years_number(files_path[1]))
+})
+
+test_that("2 files path, no year in one file name", {
+  expect_error(get_years_number(files_path[2:3]))
+})
+
+test_that("vector of file paths", {
+  expect_equal(get_years_number(files_path[1:2]), 2)
+  expect_equal(get_years_number(rep(files_path[1], 2)), 1)
+})
